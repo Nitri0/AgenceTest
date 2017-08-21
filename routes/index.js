fef03ff4bb4bfc203966bfd690a795a5cd8549ce
Homero <hhernandez@teravisiontech.com>
2017-08-21 01:56:50 -0500

    implementando redis, agregando rutinas de docker

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ let express = require('express');
 let router = express.Router();
 let http = require("axios");
 //redis-server
-//let cache = require('redis').createClient();
+let cache = require('express-redis-cache')({ client: require('redis').createClient() })
+
+
 
 const TOKEN = "b8d7ffa6fec3ba873e1cd3551aa64df7dbd4dc4f"
 
@@ -11,32 +13,34 @@ router.get('/', function(req, res, next) {
 });
 
 router.all('/get-data', function(req, res, next) {
-  //let input = req.post('country')
   let city = req.body.city || null;
-  //cache.get( city , (reply, err)=>{
-  //    if (reply){
-  //          if(util.isActiveData(reply.time)){ // hacer funcion
-  //            return res.status(200).send(response.data)
-  //          }
-  //    }
-  //    
-    
-    let now = new Date()
+  cache.get( city , (err, ress)=>{
+    if (ress.length){
+      let data = JSON.parse(ress[0].body)
 
+
+      let hourQuery = new Date(data.data.time.s).getHours()
+      let hourNow = new Date().getHours()
+
+      // verirficacion 
+      if(hourQuery == hourNow){ 
+        return res.status(200).send(data)
+      }
+    }
 
     http.get( `http://api.waqi.info/feed/${city}/?token=${TOKEN}`)
         .then((response)=>{
           if (response.data.status == "ok"){
-            //cache.set( city , response.data , (reply, err)=>{
-            // if (!err){ return res.status(200).send(reply) }
-            //  else{ return res.status(404).send(err) }
-            //})
-            return res.status(200).send(response.data);
+            response.data.data.time.s = new Date()
+            cache.add( city , JSON.stringify(response.data) , (err, entry)=>{
+              if (!err){ console.log("guardando por add");return res.status(200).send(response.data) }
+                else{console.log("error al hacer add"); return res.status(404).send(err) }
+            })
           }else{
             return res.status(404).send(response.data)
           }
         })
-  //})
+  })
 });
 
 module.exports = router;
